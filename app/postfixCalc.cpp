e0beacb300cbeb0cdb7506d5c1a519663b54de3d
mikes <mikes@ics.uci.edu>
2023-01-06 11:49:33 -0800

    Initial commit

---
+++ app/postfixCalc.cpp
@@ -0,0 +1,9 @@
+#include "postfixCalc.hpp"
+#include "LLStack.hpp"
+
+unsigned postfixCalculator(const std::vector<std::string> & entries)
+{
+
+	return 1729; // stub, not the correct answer
+}
+
---
+++ app/verifySol.cpp
@@ -0,0 +1,9 @@
+#include <unordered_map>
+#include <string>
+#include "verifySol.hpp"
+
+bool verifySolution(std::string s1, std::string s2, std::string s3, const std::unordered_map<char, unsigned> & mapping)
+{
+    return false; // FYI, this stub is not a correct solution.
+}
+
