b92e7ca3aeef61e0aeaa1f94725d20c8e8ab2f25
Eric Minassian <ehminass@uci.edu>
2023-01-12 14:59:50 -0800

    complete verifySolution

--- app/verifySol.cpp
+++ app/verifySol.cpp
@@ -1,9 +1,33 @@
-#include <unordered_map>
-#include <string>
 #include "verifySol.hpp"
 
-bool verifySolution(std::string s1, std::string s2, std::string s3, const std::unordered_map<char, unsigned> & mapping)
-{
-    return true; // FYI, this stub is not a correct solution.
-}
+#include <math.h>
 
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using std::pow;
+
+bool verifySolution(std::string s1, std::string s2, std::string s3,
+                    const std::unordered_map<char, unsigned>& mapping) {
+  unsigned int s1Num = 0, s2Num = 0, s3Num = 0;
+  for (int i = 0; i < s1.length(); i++) {
+    s1Num += mapping.at(s1[i]) * pow(10, s1.length() - i - 1);
+  }
+
+  for (int i = 0; i < s2.length(); i++) {
+    s2Num += mapping.at(s2[i]) * pow(10, s2.length() - i - 1);
+  }
+
+  for (int i = 0; i < s3.length(); i++) {
+    s3Num += mapping.at(s3[i]) * pow(10, s3.length() - i - 1);
+  }
+
+  std::cout << s1Num << " " << s2Num << " " << s3Num;
+
+  if ((s1Num + s2Num) == s3Num) {
+    return true;
+  } else {
+    return false;
+  }
+}
